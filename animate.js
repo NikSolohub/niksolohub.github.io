45a2685c6d723388996a4bb3dc04ca1249d12733
Nik Solohub <mail.niksolohub@gmail.com>
2022-05-10 12:40:24 +0300

    Update

---
+++ animate.js
@@ -0,0 +1,12 @@
+"use strict"
+{
+
+    window.anim = {
+
+        class Animate{
+            constructor(){
+
+            }
+        }
+    }
+}
--- app.js
+++ app.js
@@ -16,6 +16,75 @@
         }
     }
 
+    //Loader Display
+
+    class DisplayLoading{
+        constructor(id){
+
+            this.elem = document.querySelector('#'+id)
+
+            this._en = ()=>{
+                                //ToDo when loading is enabled
+                elem.style.display = "block"
+            }
+            this._dis = ()=>{
+                                //ToDo when loading is disabled
+                                this._smoothHide()
+            }
+
+            this._smoothHide = ()=>{
+
+                function animate({timing, draw, duration}) {
+
+                    let start = performance.now();
+                  
+                    requestAnimationFrame(function animate(time) {
+                      // timeFraction изменяется от 0 до 1
+                      let timeFraction = (time - start) / duration;
+                      if (timeFraction > 1) timeFraction = 1;
+                  
+                      // вычисление текущего состояния анимации
+                      let progress = timing(timeFraction);
+                  
+                      draw(progress); // отрисовать её
+                  
+                      if (timeFraction < 1) {
+                        requestAnimationFrame(animate);
+                      }
+                  
+                    });
+                  }
+
+
+                  animate({
+                    duration: 300,
+                    timing(timeFraction) {
+                      return timeFraction;
+                    },
+                    draw(progress) {
+                        document.querySelector('#'+id).style.opacity = 1 - progress * 1;
+                            if (progress == 1)
+                            document.querySelector('#'+id).style.display = 'none'
+                    }
+                  });
+
+
+            }
+
+        }
+
+            get enable(){
+                return this._en()
+            }
+            
+            get disable(){
+                return this._dis()
+            }
+    }
+
+                
+    //-------------------------------------------
+
 
 //MAIN FUNCTION
 function main(arg){
@@ -24,7 +93,6 @@ function main(arg){
     for(const[key, value] of Object.entries(arg.dependencies)){
         console.log(`${key} : ${value}\n`)
     }
-
         let links = document.querySelectorAll('a')
             
             links.forEach(element => {
@@ -44,7 +112,9 @@ function main(arg){
                 }
             });
 
-            
+            let Loader = new DisplayLoading('gLdr')
+                Loader.disable
+
 }
 
 
